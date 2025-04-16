b07abab23d1c350887c3be3eadba1413a61a3357
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-04-16 18:35:35 +0200

    setup enviroment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className="text-3xl font-bold underline">Subscribe to my channel</h1></div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ReservationForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReservationForm = () => {
+  return (
+    <div>
+      ReservationForm
+    </div>
+  )
+}
+
+export default ReservationForm
